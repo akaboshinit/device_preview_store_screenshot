2cc728d4b4ab8271ce2dd2c3129ea95707b3a4ab
akaboshinit <akaboshinit@gmail.com>
2024-01-19 22:51:20 +0900

    change project place

--- packages/device_preview_store_screenshot/android/app/src/main/kotlin/dev/akaboshinit/device_preview_store_screenshot/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.akaboshinit.device_preview_store_screenshot
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ packages/device_preview_store_screenshot/lib/device_preview_store_screenshot.dart
@@ -0,0 +1,7 @@
+library device_preview_store_screenshot;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
